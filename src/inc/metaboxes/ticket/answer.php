8d9934a719b463e33b32731b54bedc424a77841c
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-11-27 08:11:43 -0300

    translate answer

--- src/inc/metaboxes/ticket/answer.php
+++ src/inc/metaboxes/ticket/answer.php
@@ -13,20 +13,20 @@ function sts_metabox_answer_render( $post ) {
 	?>
 	<div class="ticket-answer">
 		<section>
-			<label for="answer-subject"><?php _e( 'Subject', 'support-ticket' ); ?></label>
+			<label for="answer-subject"><?php _e( 'Assunto', 'support-ticket' ); ?></label>
 			<div class="input-pre">
 				<span>Re: [Ticket #<?php echo $_GET['ID']; ?>]</span>
 				<input id="answer-subject" name="t[subject]" type="text" />
 			</div>
 		</section>
 		<section>
-			<label for="answer-answer"><?php _e( 'Message', 'support-ticket' ); ?></label>
+			<label for="answer-answer"><?php _e( 'Mensagem', 'support-ticket' ); ?></label>
 			<div>
 				<textarea id="answer-answer" name="t[answer]"></textarea>
 			</div>
 		</section>
 		<section>
-			<button class="button button-primary button-large"><?php _e( 'Answer', 'support-ticket' ); ?></button>
+			<button class="button button-primary button-large"><?php _e( 'Responder', 'support-ticket' ); ?></button>
 		</section>
 	</div>
 	<?php
